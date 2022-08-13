316dbc7184ebc7bc2f12d60c9364e8b9903a584a
A-Taak <110165904+A-Taak@users.noreply.github.com>
2022-08-13 13:20:45 +0100

    test: see how new branch is handled. (#1)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello welcome to your first AWS/Github site
+          This is new branch test to see how actions work.
         </p>
         <a
           className="App-link"
